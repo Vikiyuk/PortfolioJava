4995048440ea3661485617ceeab2d95e4ba5d45e
FiFol <kasztelacen@gmail.com>
2025-08-11 13:48:33 +0000

    statistics methods added

--- PortfolioBackend/src/Portfolio.java
+++ PortfolioBackend/src/Portfolio.java
@@ -1,6 +1,4 @@
-package PortfolioBackend.src;
-
-
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class Portfolio {
@@ -10,18 +8,36 @@ public class Portfolio {
         list.add(stock);
     }
 
-    public void showPortfolio () {
-        for (Stock stock : list) {
-            System.out.println(stock.getSymbol() + " " + stock.getQuantity() + " " + stock.getPricePerShare());
-        }
+    public static BigDecimal calculateValue (Stock stock) {
+        return stock.getQuantity().multiply(stock.getPricePerShare());
     }
 
+    public void showPortfolio () {
+        BigDecimal totalValue = new BigDecimal(0);
+        for (Stock stock : list) {
+            System.out.println(stock.getSymbol() + " " + stock.getQuantity() + " " + stock.getPricePerShare());
+            totalValue = totalValue.add(Portfolio.calculateValue(stock));
+        }
+        System.out.println("Total value: " + totalValue);
+    }
+
+
+
+
+    // if -1, leave as it is previously in terms of quantity and price
     public void updateStock (Stock stock) {
         String name = stock.getSymbol();
+        BigDecimal newPrice = stock.getPricePerShare();
+        BigDecimal newQuantity = stock.getQuantity();
+
         for (Stock temp : list) {
             if (temp.getSymbol().equals(name)) {
-                temp.setQuantity(stock.getQuantity());
-                temp.setPricePerShare(stock.getPricePerShare());
+                if(!(newQuantity.equals(new BigDecimal(-1)))) {
+                    temp.setQuantity(newQuantity);
+                }
+                if (!(newPrice.equals(new BigDecimal(-1)))) {
+                    temp.setPricePerShare(stock.getPricePerShare());
+                }
             }
         }
     }
@@ -29,4 +45,5 @@ public class Portfolio {
     public void removeStock(Stock stock) {
         list.remove(stock);
     }
+
 }
