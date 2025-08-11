2ecae04a1af95d8c6f0a76405d46f5c2055bc3b9
Joanka <joanka@test.com>
2025-08-11 12:29:10 +0000

    Create Stock class

---
+++ PortfolioBackend/src/Stock.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+
+public class Stock {
+    private String symbol;
+    private BigDecimal quantity;
+    private BigDecimal pricePerShare;
+
+
+    public Stock(String symbol, BigDecimal quantity, BigDecimal pricePerShare) {
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.pricePerShare = pricePerShare;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPricePerShare() {
+        return pricePerShare;
+    }
+
+    public void setPricePerShare(BigDecimal pricePerShare) {
+        this.pricePerShare = pricePerShare;
+    }
+}
